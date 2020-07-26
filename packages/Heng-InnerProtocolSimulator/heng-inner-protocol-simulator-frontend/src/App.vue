80e1e9306d64cfcee4273f2f5e2330f2f4723f41
Hg@SurfaceBook <1395943920@qq.com>
2020-07-26 09:17:38 +0800

    初始化内部协议模拟器前端

---
+++ packages/Heng-InnerProtocolSimulator/heng-inner-protocol-simulator-frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <basicmessage />
+    </div>
+</template>
+
+<script lang="ts">
+import basicmessage from "./components/message/basicmassage.vue";
+
+export default {
+    name: "App",
+    components: {
+        basicmessage,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ packages/Heng-InnerProtocolSimulator/heng-inner-protocol-simulator-frontend/src/components/message/basicmassage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>这里应当有个消息</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "basicmessage",
+});
+</script>
+
+<style lang="stylus" scoped></style>
