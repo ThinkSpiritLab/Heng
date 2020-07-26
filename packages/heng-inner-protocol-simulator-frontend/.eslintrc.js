1b94ad09ef3bdc6fd0f4728d3bb7157476b333c1
Hg@SurfaceBook <1395943920@qq.com>
2020-07-26 17:19:32 +0800

    又一次修正错误

--- packages/heng-inner-protocol-simulator-frontend/.eslintrc.js
+++ packages/heng-inner-protocol-simulator-frontend/.eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
     root: true,
     // parser: "@typescript-eslint/parser",
-    parser: 'vue-eslint-parser',
+    parser: "vue-eslint-parser",
     plugins: [
         // "@typescript-eslint",
     ],
