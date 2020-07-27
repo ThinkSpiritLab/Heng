d805dd5344004133359223d8c02575788a66a83a
HuGuang@NewDesktop <1395943920@qq.com>
2020-07-27 22:19:16 +0800

    植入消息框架

--- packages/heng-inner-protocol-simulator-frontend/src/components/judger-simulator/judger-simulator.vue
+++ packages/heng-inner-protocol-simulator-frontend/src/components/judger-simulator/judger-simulator.vue
@@ -9,7 +9,20 @@
         >{{ tab }}</button>-->
         <tabselect v-bind:tabs="tabs" v-bind:select="(s) => (activetab = s)" />
         <div v-if="activetab === 'message'" class="tab">
-            <p>TODO</p>
+            <tabselect
+                v-bind:tabs="messages.tabs"
+                v-bind:select="(s) => (messages.curtab = s)"
+            />
+            <div v-if="messages.curtab === 'send'">
+                <messages v-bind:messages="messages.send" />
+                <sendpanel
+                    v-bind:connection="connection"
+                    v-bind:send="(msg) => send(msg)"
+                />
+            </div>
+            <div v-if="messages.curtab === 'received'">
+                <messages v-bind:messages="messages.received" />
+            </div>
         </div>
         <div v-else-if="activetab === 'server'" class="tab">
             <p>服务器连接面板</p>
@@ -22,7 +35,10 @@
                 <input v-model.number="port" placeholder="8080" />
             </p>
             <button v-on:click="connect">连接</button>
-            <button v-on:click="connection.close()">断开</button>
+            <button v-on:click="connection.close()">
+                断开
+            </button>
+            <p v-if="connection != undefined">{{ connection.readyState }}</p>
         </div>
         <div v-else-if="activetab === 'setstatus'" class="tab">
             <tabselect
@@ -47,6 +63,8 @@
 import Vue from "vue";
 import hardware from "./hardware.vue";
 import tabselect from "../tabselect.vue";
+import messages from "../message/messages.vue";
+import sendpanel from "../message/sendpanel.vue";
 export default Vue.extend({
     name: "judgersimulator",
     data: function () {
@@ -55,11 +73,17 @@ export default Vue.extend({
             connected: false,
             activetab: "server",
             tabs: ["server", "message", "setstatus"],
-            server: "0.0.0.0",
-            port: 8080,
+            server: "echo.websocket.org",
+            port: 80,
             serverpublickey: "",
             judgerprivatekey: "",
             judgerkeynumber: "",
+            messages: {
+                curtab: "send",
+                tabs: ["send", "received"],
+                send: [],
+                received: [],
+            },
             status: {
                 activetab: "cpu",
                 tabs: ["cpu", "memory"],
@@ -77,19 +101,41 @@ export default Vue.extend({
     methods: {
         connect: function () {
             let url = "ws://" + this.server + ":" + this.port;
+            let that = this;
             console.log("connecting to " + url);
             this.connection = new WebSocket(url);
             this.connection.onopen = function () {
-                alert("connected");
+                alert("connection opened");
+            };
+            this.connection.onclose = function () {
+                alert("connection closed");
+            };
+            this.connection.onmessage = function (msg) {
+                let str = msg.data;
+                console.log(str);
+                let data = JSON.parse(str);
+                that.messages.received.push(data);
             };
             this.connection.onerror = function (e) {
                 alert("Error: " + JSON.stringify(e));
             };
         },
+        send: function (msg) {
+            let str = JSON.stringify(msg);
+            if (this.connection != undefined) {
+                console.log("send" + str);
+                this.connection.send(str);
+            } else {
+                alert("not connected!");
+                console.log("not connected when send " + str);
+            }
+        },
     },
     components: {
         hardware,
         tabselect,
+        messages,
+        sendpanel,
     },
 });
 </script>
